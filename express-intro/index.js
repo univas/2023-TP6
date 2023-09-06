9515625e62bc6da3ed158ba4bca6a1635668ab8f
Marcos Antônio dos Santos <marcaosi2014@gmail.com>
2023-09-05 21:33:31 -0300

    Add files via upload

--- express-intro/index.js
+++ express-intro/index.js
@@ -4,6 +4,12 @@ const express = require("express")
 // instanciando o aplicativo
 const app = express()
 
+// Forneça na URL /bootstrap os arquivos de forma estática
+app.use('/bootstrap', express.static(__dirname + "/node_modules/bootstrap/dist"))
+
+// Adicionando middleware para ensinar o express a tratar o corpo da requisição enviado por formulário via post
+app.use(express.urlencoded())
+
 // configurando o ejs
 app.set("view engine", "ejs")
 
@@ -21,10 +27,22 @@ app.get("/site", (req, res) => {
 // rota para o site usando ejs
 app.get("/site2", (req, res) => {
     // obtém o parâmetro passado na query string
-    const { count } = req.query 
+    const { count, nome, sobrenome } = req.query 
     // constrói o html a partir de um template e devolve para ser renderizado.
     // no segundo parâmetro é enviado um objeto contendo os dados para a página
-    res.render("index", {usuario: "josecarlos2", count})
+    res.render("index", {usuario: "josecarlos2", count, nomecompleto: `${nome} ${sobrenome}`})
+})
+
+// Criando rota para receber requisições POST em /site2
+app.post("/site2", (req, res) => {
+    const { count, nome, sobrenome } = req.body
+
+    res.render("resultado", { nomecompleto: `${nome} ${sobrenome}` })
+})
+
+// Criando rota para receber a pagina como parametro
+app.get("/site2/:pagina", (req, res) => {
+    res.render(req.params.pagina)
 })
 
 // configurando aplicativo para funcionar na porta 3000
