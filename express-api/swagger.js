cf26b537437fc67cf5b7093d549fc06adb95d01d
Marcos Antônio dos Santos <marcaosi2014@gmail.com>
2023-10-25 19:21:14 -0300

    Add files via upload

--- express-api/swagger.js
+++ express-api/swagger.js
@@ -1,10 +1,14 @@
+// Importação da biblioteca para gerar a documentação automaticamente
 const autogen = require("swagger-autogen")()
 
+// Definição do arquivo resultado da documentação
 const fileDocs = './swaggerOutput.json'
 
+// Definição dos arquivos que mapeam as rotas da aplicação
 const routeFiles = [
     './rotas/usuarios.js',
     './rotas/token.js'
 ]
 
+// Geração da documentação automaticamente
 autogen(fileDocs, routeFiles)
